73b954af3632c88135074a9f7d619f1d755f7cc3
Nikhil Patidar <66742645+nikhilpatidar@users.noreply.github.com>
2021-08-04 23:09:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,24 @@ from pyrogram import Client
 from config import Config
 from config import LOGGER
 
-class Bot(Client):
-    def __init__(self):
-        super().__init__(
-            "mybot",
-            bot_token=Config.TG_BOT_TOKEN,
-            api_id=Config.APP_ID,
-            api_hash=Config.API_HASH,
-        )
-        self.LOGGER = LOGGER
+app = Client(
+    "droplink",
+    bot_token=Config.TG_BOT_TOKEN,
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+)
 
 api_token = "e0decc69310ffbf915d46649b32fdfe6941a0d73"
 
 
 
-@Bot.on_message(filters.private & filters.command(['start']))
+@app.on_message(filters.private & filters.command(['start']))
 async def start(client,message):
   await message.reply_text(f"Hello {message .from_user.first_name}\nI am Droplink.co short link genrator.", reply_to_message_id = message.message_id)
 
 
 
-@Bot.on_message(filters.private & filters.regex("http|https"))
+@app.on_message(filters.private & filters.regex("http|https"))
 async def kuklink(client,message):
   long_url = message.text
   try:
@@ -35,7 +32,4 @@ async def kuklink(client,message):
     await message.reply_text(f"```{result}```", reply_to_message_id= message.message_id)
   except Exception as e :
     await message.reply_text(e)
-    
-    
-app = Bot()
-app.run()    
+app.run() 
