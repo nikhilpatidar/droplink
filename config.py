1b57a05a5968a5da01b0f7da0368e9a6733022bb
Nikhil Patidar <66742645+nikhilpatidar@users.noreply.github.com>
2022-10-22 16:39:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     APP_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
-
+    API_TOKEN = os.environ.get("API_TOKEN", "")
 
 
 def LOGGER(name: str) -> logging.Logger:
